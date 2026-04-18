ba3d465e688931ac2c1bc9db6de427cd1017dab2
Badr Matrane <matrane.badr@gmail.com>
2026-04-19 01:12:48 +0200

    fix postgres connnection string

--- LoveCapsule.Api/Program.cs
+++ LoveCapsule.Api/Program.cs
@@ -2,11 +2,16 @@ using LoveCapsule.Api.Services;
 using LoveCapsule.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+var databaseUrl = Environment.GetEnvironmentVariable("DB_CONNECTION");
+var uri = new Uri(databaseUrl);
+var userInfo = uri.UserInfo.Split(':');
 QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString =
+    $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.Trim('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
