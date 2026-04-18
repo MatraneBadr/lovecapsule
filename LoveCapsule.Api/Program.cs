e5f5bb9e9aa19c59dc9dc6ac75946a1450261450
Badr Matrane <matrane.badr@gmail.com>
2026-04-18 23:32:11 +0200

    enable swagger in prod

--- LoveCapsule.Api/Program.cs
+++ LoveCapsule.Api/Program.cs
@@ -25,12 +25,10 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 app.UseCors();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 
 app.UseHttpsRedirection();
