23196107736770db070727dcbd3b919e3c9669e0
Badr Matrane <matrane.badr@gmail.com>
2026-04-21 22:36:48 +0200

    init Migrations

---
+++ LoveCapsule.Infrastructure/Migrations/20260421203108_InitClean.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoveCapsule.Infrastructure.Migrations
+{
+    public partial class InitClean : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
